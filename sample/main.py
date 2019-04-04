1f88ce5bb146d4fdafd487cab5834d0335455c87
stasbar <stasbar1995@gmail.com>
2019-04-04 23:38:24 +0200

    init commit

---
+++ sample/main.py
@@ -0,0 +1,6 @@
+def main():
+    print('main function')
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_basic.py
@@ -0,0 +1,7 @@
+
+# added to demonstrate `find_packages` example in
+# setup.py that excludes installing the "tests" package
+
+
+def test_success():
+    assert True
