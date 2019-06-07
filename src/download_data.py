eb393165338b42d6e3c66e700212aa12e348785e
stasbar <stasbar1995@gmail.com>
2019-06-07 02:39:11 +0200

    szum3 metrics
    Took 2 hours 0 minutes

--- src/download_data.py
+++ src/download_data.py
@@ -1,6 +1,8 @@
+import keras
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from keras import metrics, losses
 from keras.layers import Dense
 from keras.layers import Dropout
 from keras.layers import LSTM
@@ -18,6 +20,14 @@ chart_names = ["total-bitcoins", "market-price", "market-cap", "trade-volume", "
 
 chart_names2 = ["market-price"]
 
+my_metrics = [metrics.binary_accuracy,
+              metrics.mean_absolute_error,
+              metrics.sparse_categorical_accuracy,
+              losses.mean_absolute_percentage_error,
+              losses.squared_hinge,
+              losses.hinge,
+              losses.poisson]
+
 
 def plot(x_list, y_list, y_list2, chart_name):
     plt.plot(x_list, y_list, label='line1')
@@ -32,7 +42,8 @@ def create_model():
     model.add(LSTM(units=64, input_shape=(x_train.shape[1], x_train.shape[2])))
     model.add(Dropout(rate=0.3))
     model.add(Dense(1))
-    model.compile(optimizer='adagrad', loss='mse')
+    model.compile(optimizer='adagrad', loss='mse',
+                  metrics=my_metrics)
     return model
 
 
@@ -45,86 +56,96 @@ def train_model(model):
     plt.xlabel('Epoch')
     plt.legend(['Train', 'Val'], loc='upper left')
     plt.show()
-    model.save('MODEL_SPLIT1.h5')
 
 
-normalization = True
-data_2018 = pd.read_csv('bitcoin_market_data.csv', sep=',')
+def predict(xs, ys, name):
+    predicted = model.predict(xs)
+    if normalization:
+        predicted = test_scaller2.inverse_transform(predicted)
+    score = model.evaluate(x=xs, y=ys, verbose=2)
 
-total_data = pd.concat([data_2018]).iloc[:, 2:5].values
+    # prepend scores with default loss function
+    my_metrics.insert(0, losses.mean_squared_error)
+    print(f'--------- {name} -----------')
+    [print(f'{my_metrics[index].__name__}: {item}') for index, item in enumerate(score)]
+    my_metrics.pop()
 
-train_set_size = int(0.9 * total_data.shape[0])
-val_set_size = train_set_size + int(0.05 * total_data.shape[0])
-test_set_size = val_set_size + int(0.05 * total_data.shape[0]) + 1
+    x_series = list(range(0, predicted.shape[0]))
+    x_series = np.reshape(x_series, (x_series.__len__(), 1))
+    if normalization:
+        plot(x_series, predicted, test_scaller2.inverse_transform(ys.reshape(-1, 1)), name)
+    else:
+        plot(x_series, predicted, ys.reshape(-1, 1), name)
 
-train_set = total_data[:train_set_size, :]
-val_set = total_data[train_set_size:val_set_size, :]
-test_set = total_data[val_set_size:test_set_size, :]
-test_set2 = test_set.copy()[:, 0]
 
-train_scaller = MinMaxScaler(feature_range=(0, 1))
-if normalization:
-    train_set = train_scaller.fit_transform(train_set)
+if __name__ == '__main__':
+    normalization = True
+    data_2018 = pd.read_csv('bitcoin_market_data.csv', sep=',')
 
-val_scaller = MinMaxScaler(feature_range=(0, 1))
-if normalization:
-    val_set = val_scaller.fit_transform(val_set)
+    total_data = pd.concat([data_2018]).iloc[:, 2:5].values
 
-test_scaller = MinMaxScaler(feature_range=(0, 1))
-if normalization:
-    test_set = test_scaller.fit_transform(test_set)
+    train_set_size = int(0.9 * total_data.shape[0])
+    val_set_start = train_set_size + int(0.05 * total_data.shape[0])
+    test_set_start = val_set_start + int(0.05 * total_data.shape[0]) + 1
 
-test_scaller2 = MinMaxScaler(feature_range=(0, 1))
-if normalization:
-    test_set2 = test_set2.reshape(-1, 1)
-    test_set2 = test_scaller2.fit_transform(test_set2)
+    train_set = total_data[:train_set_size, :]
+    # val_set = total_data[:train_set_size:int(1/0.05), :]
+    val_set = total_data[train_set_size:val_set_size, :]
+    test_set = total_data[val_set_size:test_set_size, :]
+    test_set2 = test_set.copy()[:, 0]
 
-x_train = []
-y_train = []
-x_val = []
-y_val = []
-x_test = []
-y_test = []
+    train_scaller = MinMaxScaler(feature_range=(0, 1))
+    if normalization:
+        train_set = train_scaller.fit_transform(train_set)
 
-vector_size = 128
-for i in range(vector_size, train_set.shape[0]):
-    x_train.append(train_set[i - vector_size:i, :])
-    y_train.append(train_set[i, 0])
+    val_scaller = MinMaxScaler(feature_range=(0, 1))
+    if normalization:
+        val_set = val_scaller.fit_transform(val_set)
 
-x_train, y_train = np.array(x_train), np.array(y_train)
-for i in range(vector_size, val_set.shape[0]):
-    x_val.append(val_set[i - vector_size:i, :])
-    y_val.append(val_set[i, 0])
+    test_scaller = MinMaxScaler(feature_range=(0, 1))
+    if normalization:
+        test_set = test_scaller.fit_transform(test_set)
 
-x_val, y_val = np.array(x_val), np.array(y_val)
+    test_scaller2 = MinMaxScaler(feature_range=(0, 1))
+    if normalization:
+        test_set2 = test_set2.reshape(-1, 1)
+        test_set2 = test_scaller2.fit_transform(test_set2)
 
-for i in range(vector_size, test_set.shape[0]):
-    x_test.append(test_set[i - vector_size:i, :])
-    y_test.append(test_set[i, 0])
+    x_train = []
+    y_train = []
+    x_val = []
+    y_val = []
+    x_test = []
+    y_test = []
 
-x_test, y_test = np.array(x_test), np.array(y_test)
+    vector_size = 128
+    for i in range(vector_size, train_set.shape[0]):
+        x_train.append(train_set[i - vector_size:i, :])
+        y_train.append(train_set[i, 0])
 
-x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], x_train.shape[2]))
-x_val = np.reshape(x_val, (x_val.shape[0], x_val.shape[1], x_val.shape[2]))
-x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], x_test.shape[2]))
+    x_train, y_train = np.array(x_train), np.array(y_train)
+    for i in range(vector_size, val_set.shape[0]):
+        x_val.append(val_set[i - vector_size:i, :])
+        y_val.append(val_set[i, 0])
 
-model = create_model()
+    x_val, y_val = np.array(x_val), np.array(y_val)
 
-# train or load, don't do both !
-# train_model(model)
-model.load_weights('MODEL_SPLIT2.h5')
+    for i in range(vector_size, test_set.shape[0]):
+        x_test.append(test_set[i - vector_size:i, :])
+        y_test.append(test_set[i, 0])
 
-predicted = model.predict(x_val)
-if normalization:
-    predicted = test_scaller2.inverse_transform(predicted)
+    x_test, y_test = np.array(x_test), np.array(y_test)
 
-evaluate = model.evaluate(x=x_val, y=y_val)
+    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], x_train.shape[2]))
+    x_val = np.reshape(x_val, (x_val.shape[0], x_val.shape[1], x_val.shape[2]))
+    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], x_test.shape[2]))
 
-print(evaluate)
+    model = create_model()
 
-x_series = list(range(0, predicted.shape[0]))
-x_series = np.reshape(x_series, (x_series.__len__(), 1))
-if normalization:
-    plot(x_series, predicted, test_scaller2.inverse_transform(y_val.reshape(-1, 1)), "pred - one shot")
-else:
-    plot(x_series, predicted, y_val.reshape(-1, 1), "pred - one shot")
+    # train or load, don't do both !
+    # train_model(model)
+    model.load_weights('MODEL_SPLIT2.h5')
+
+    predict(x_train, y_train, "Train")
+    predict(x_val, y_val, "Val")
+    predict(x_test, y_test, "Test")
